6f498b7415dd92fcec5d74498bb9e7ebe394893c
Jervie Marquez <jerviemarquez@gmail.com>
2025-05-09 03:15:01 +0800

    fixing github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jerviechronix.github.io/',
-})
+  base: '/jerviechronix.github.io/', // Keep '/' if deploying to your main GitHub page (your-username.github.io)
+});
